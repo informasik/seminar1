6518db0302e21315260e65aa4abdcdb03da37073
informasik <asset.dyussenbayev@gmail.com>
2019-02-08 23:42:39 +0600

    new

--- myclass.py
+++ myclass.py
@@ -1,20 +1,11 @@
 class Hello:
 	def __init__(self):
-		print("Today.")	
+		print("Number")	
 	
-	def ask(self):
-		print("How are you?")
+	def one(self):
+		print("First")
 
-	def ans(self):
-		print("Fine.")
-
-
-def main():
-	say=Hello()
-	say.ask()
-	say.ans()
-
-if __name__ == "__main__":
-	main()
+	def two(self):
+		print("Second")
 
 
